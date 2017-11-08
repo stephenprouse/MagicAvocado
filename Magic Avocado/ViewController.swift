f8a44d2c660b0f1422d0f3d48216b963d579459d
Stephen Prouse <prouse@yahoo.com>
2017-11-08 15:30:33 -0500

    Initial Commit

---
+++ Magic Avocado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic Avocado
+//
+//  Created by Stephen Prouse on 11/8/17.
+//  Copyright © 2017 Stephen Prouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
