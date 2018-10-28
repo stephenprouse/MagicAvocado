055f65c9e4a5a371f89d97987c01ad6feba08fdd
Stephen Prouse <prouse@yahoo.com>
2018-10-28 09:18:09 -0400

    Initial Commit

--- Magic Avocado/ViewController.swift
+++ Magic Avocado/ViewController.swift
@@ -7,12 +7,86 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelMessage: UILabel!
+
+    @IBAction func clearButton(_ sender: Any) {
+        labelMessage.text = ""
+        explosion.stopAnimating()
+        
+    }
+    
+    @IBOutlet weak var explosion: UIImageView!
+    
+    var buttonBeep: AVAudioPlayer?
+    func playSound() {
+        let path = Bundle.main.path(forResource: "blart", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            buttonBeep = try AVAudioPlayer(contentsOf: url)
+            buttonBeep?.play()
+            print("playing " + path)
+        } catch {
+            print(error)
+        }
+    }
+    
+    var sound: Bool = true
+    var vibrate: Bool = true
+    var messages: [String] =
+        ["Yes",
+         "No",
+         "Doubtful",
+         "Maybe",
+         "Yeah right",
+         "Don't be rediculous",
+         "Seriously? No",
+         "Not a chance",
+         "Count on it",
+         "Someday probably",
+         "Absolutely",
+         "Without a doubt",
+         "Ask me again",
+         "I can't say",
+         "Sure why not"]
+    
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        print("Device was shaken!")
+        let randomNumber = arc4random_uniform(15)
+        print(randomNumber)
+        labelMessage.text = messages[(Int(randomNumber))]
+        
+        // Send vibrate
+        AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+        print("Device vibrated!")
+        
+        // Play sound
+        playSound()
+        print("Sound played!")
+        
+        // Display animation
+        explosion.animationImages =
+            [UIImage(named: "explosion1.png")!,
+             UIImage(named: "explosion2.png")!,
+             UIImage(named: "explosion3.png")!,
+             UIImage(named: "explosion4.png")!,
+             UIImage(named: "explosion5.png")!,
+             UIImage(named: "explosion6.png")!
+        ]
+        explosion.animationDuration = 0.4
+        explosion.startAnimating()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
